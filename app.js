844c29f7700cfbaca5b833ec4f1c36ad9c674642
Esteban Benito <ebenito@outlook.com>
2020-07-09 18:18:20 +0200

    archivo configuración Mongoose

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
+require('./config/mongoose');
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-let cnn = 'mongodb+srv://sa:TKVg6qgR14D5c6my@clusterxtv.qfdke.azure.mongodb.net/pruebas?retryWrites=true&w=majority';
 
 var app = express();
-
-require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-  console.log('IP servidor: '+add);
-})
-
-//Conectamos a MongoDB; en producción creamos la variable de entorno con la cadena de conexión en producción, que incluye la password (asi no se verá publicamente al estar publicado en GIT) y en desarrollo usará la BD local.
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/pruebas'; 
-//console.log('Intentado conectar a MongoBD: ' + MONGO_URI);
-
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
- .then(()=>console.log('Conectado con éxito a MongoDB'))
- .catch(console.error);
-
+ 
 // view engine setup
 app.set('view engine', 'jade');
 
---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+  //Conectamos a MongoDB; en producción creamos la variable de entorno con la cadena de conexión en producción, que incluye la password (asi no se verá publicamente al estar publicado en GIT) y en desarrollo usará la BD local.
+  const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/pruebas'; //'mongodb+srv://sa:TKVg6qgR14D5c6my@clusterxtv.qfdke.azure.mongodb.net/pruebas?retryWrites=true&w=majority'
+
+  console.log('Intentado conectar a MongoBD: ' + MONGO_URI);
+
+  require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+    console.log('Desde el servidor con IP: '+add);
+  })
+    
+  mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+   .then(()=>console.log('Conectado con éxito a MongoDB'))
+   .catch(console.error);
