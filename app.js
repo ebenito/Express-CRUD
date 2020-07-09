93b42c4e596cd91fc18a0c304501ad7ac71f5515
Esteban Benito <ebenito@outlook.com>
2020-07-09 09:32:26 +0200

    mensaje con IP servidor

--- app.js
+++ app.js
@@ -11,9 +11,13 @@ let cnn = 'mongodb+srv://sa:TKVg6qgR14D5c6my@clusterxtv.qfdke.azure.mongodb.net/
 
 var app = express();
 
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+  console.log('IP servidor: '+add);
+})
+
 //Conectamos a MongoDB; en producción creamos la variable de entorno con la cadena de conexión en producción, que incluye la password (asi no se verá publicamente al estar publicado en GIT) y en desarrollo usará la BD local.
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/pruebas'; 
-console.log('Intentado conectar a MongoBD: ' + MONGO_URI);
+//console.log('Intentado conectar a MongoBD: ' + MONGO_URI);
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
  .then(()=>console.log('Conectado con éxito a MongoDB'))
